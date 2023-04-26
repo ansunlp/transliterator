79186c465345d01403d9f7dc15790e870ce8712c
ansunlp <119314985+ansunlp@users.noreply.github.com>
2023-04-26 17:20:35 +0530

    Update setup.py

--- app/setup.py
+++ app/setup.py
@@ -17,16 +17,16 @@ with pathlib.Path('dependencies.txt').open() as requirements_txt:
 
 # Read meta-data
 about = {}
-exec(open('ai4bharat/transliteration/__metadata.py').read(), about)
+exec(open('ansutr/transliteration/__metadata.py').read(), about)
 
 # This call to setup() does all the work
 setup(
-    name="ai4bharat-transliteration",
+    name="ansu-transliteration",
     version=about["__version__"],
     description="Indic-Xlit: Transliteration library for Indic Languages. Conversion of text from English to 21 languages of South Asia.",
     long_description=README,
     long_description_content_type="text/markdown",
-    url="https://github.com/AI4Bharat/IndicXlit",
+    url="https://github.com/ansunlp/transliterator",
     project_urls={
         # 'Say Thanks': 'mailto:opensource@ai4bharat.org',
         'Our Research': 'https://ai4bharat.org/transliteration',
@@ -34,11 +34,11 @@ setup(
         'Report Issues': 'https://github.com/AI4Bharat/IndicXlit/issues',
         'Source Code': 'https://github.com/AI4Bharat/IndicXlit/tree/master/app',
     },
-    author="AI4Bhārat",
-    author_email="opensource@ai4bharat.org",
-    # packages=["ai4bharat.transliteration"],
+    author="Sanjana",
+    author_email="ansunlp123@gmail.com",
+    packages=["ansu.transliteration"],
     packages=find_packages(exclude=("tests",)),
-    # packages=find_packages(include=('ai4bharat.transliteration*')),
+    packages=find_packages(include=('ansu.transliteration*')),
     include_package_data=True,
     install_requires=install_requires,
     python_requires='>=3.6',
